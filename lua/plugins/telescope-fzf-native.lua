0c2484130b3146c6fc7417a15a9d2e639c07ac5b
code-bullett <127530534+code-bullett@users.noreply.github.com>
2025-02-25 07:52:44 -0300

    changed the telescope to work on windows

--- lua/plugins/telescope-fzf-native.lua
+++ lua/plugins/telescope-fzf-native.lua
@@ -1,8 +1,1 @@
-return {
-	"nvim-telescope/telescope-fzf-native.nvim",
-	build = "make",
-	opts = {},
-	config = function()
-		require("telescope").load_extension("fzf")
-	end,
-}
+{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
