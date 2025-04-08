818f9c961a952278599b10e2c2147f68caab3d42
Adriel Frezatti <Adrielfrezatti@gmail.com>
2025-04-08 20:43:59 -0300

    trouble and finally setting but telescop

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,45 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+
+			telescope.setup({
+				-- Your Telescope setup configuration here
+			})
+
+			-- Keymap definitions for Telescope built-in pickers
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		end,
 	},
-	ops = {
-		ensure_installed = {},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				extensions = {
+					file_browser = {
+						-- Your Telescope File Browser setup configuration here
+						hijack_netrw = true, -- Optionally disable netrw
+					},
+				},
+			})
+			telescope.load_extension("file_browser")
+
+			-- Keymap for launching Telescope File Browser
+			vim.keymap.set(
+				"n",
+				"<leader>fd",
+				":Telescope file_browser<CR>",
+				{ noremap = true, desc = "Telescope File Browser" }
+			)
+		end,
 	},
 }
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- Your Trouble.nvim configuration options here
+	},
+	config = function()
+		local trouble = require("trouble")
+
+		-- Key mappings
+		vim.keymap.set("n", "<leader>xx", function()
+			trouble.toggle("diagnostics")
+		end, { desc = "Toggle diagnostics (Trouble)" })
+		vim.keymap.set("n", "<leader>xX", function()
+			trouble.toggle("diagnostics", { filter = { buf = 0 } })
+		end, { desc = "Toggle buffer diagnostics (Trouble)" })
+		vim.keymap.set("n", "<leader>cs", function()
+			trouble.toggle("symbols", { focus = false })
+		end, { desc = "Toggle symbols (Trouble)" })
+		vim.keymap.set("n", "<leader>cl", function()
+			trouble.toggle("lsp", { focus = false, win = { position = "right" } })
+		end, { desc = "Toggle LSP definitions/references (Trouble)" })
+		vim.keymap.set("n", "<leader>xL", function()
+			trouble.toggle("loclist")
+		end, { desc = "Toggle location list (Trouble)" })
+		vim.keymap.set("n", "<leader>xQ", function()
+			trouble.toggle("quickfix")
+		end, { desc = "Toggle quickfix list (Trouble)" })
+	end,
+}
