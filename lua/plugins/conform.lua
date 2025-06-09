888cbdd4ca27e577e9fa1c72c30fde610abcb0aa
frezatti <68041159+frezatti@users.noreply.github.com>
2025-06-09 14:41:55 -0300

    Update conform.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,38 @@
+-- lua/plugins/conform.lua
+
 return {
 	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Add other filetypes and their respective formatters here
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
+	lazy = true,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "n",
+			desc = "Format buffer",
 		},
 	},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "goimports", "gofumpt", "golines" },
+				python = { "isort", "black" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				yaml = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+		})
+	end,
 }
