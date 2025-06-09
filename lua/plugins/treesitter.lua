5e7867a05f631b215c140bc3121e8a0bd40c7e98
frezatti <68041159+frezatti@users.noreply.github.com>
2025-06-09 14:44:50 -0300

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,20 @@
+-- lua/plugins/treesitter.lua
+
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	ops = {
-		-- A list of parser names, or "all" (the five listed parsers should always be installed)
-		ensure_installed = { "javascript", "go", "typescript", "rust", "cpp", "c", "lua", "vim", "vimdoc", "query" },
-
-		sync_install = false,
-
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-	},
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  -- The 'opts' table will be passed to the plugin's .setup() function automatically.
+  opts = {
+    ensure_installed = { "javascript", "go", "typescript", "rust", "cpp", "c", "lua", "vim", "ruby","vimdoc", "query" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+  },
+  config = function(_, opts)
+    -- The 'config' function is still needed here to call the setup function from the correct module.
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
