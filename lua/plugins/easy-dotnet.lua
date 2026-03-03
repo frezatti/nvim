d9fcde39b890ddd536f50277bdc07bee1332308e
frezatti <adrielfrezatti@gmail.com>
2026-03-03 08:39:45 -0300

    mostly adding plugins to help with C# development

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -4,15 +4,13 @@ return {
 	keys = {
 		{ "<leader>cb", "<cmd>Dotnet build<cr>", desc = ".NET Build" },
 		{ "<leader>cr", "<cmd>Dotnet run<cr>", desc = ".NET Run" },
+		{ "<leader>cc", "<cmd>Dotnet clean<cr>", desc = ".NET Clean" },
 		{ "<leader>ct", "<cmd>Dotnet test<cr>", desc = ".NET Test" },
-		{ "<leader>cp", "<cmd>Dotnet project<cr>", desc = ".NET Project Manager" },
+		{ "<leader>cT", "<cmd>Dotnet testrunner<cr>", desc = ".NET TestRunner" },
+		{ "<leader>cp", "<cmd>Dotnet project view<cr>", desc = ".NET Project Manager" },
 	},
 	config = function()
 		require("easy-dotnet").setup({
-			test_runner = {
-				view_command = "neotest",
-			},
-			-- Automatically find the .sln or .csproj file in your root directory
 			auto_bootstrap_namespace = true,
 		})
 	end,
